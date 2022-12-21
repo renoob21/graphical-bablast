17c31c8dbd2d0c0287b9086924abc2f54afb16b9
renoob21 <reno.iqbalsah21@gmail.com>
2022-12-21 11:27:26 +0700

    deleting unused libraries

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
-from pynput.keyboard import Key, Controller
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
